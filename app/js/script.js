37ae60c9a807d868470f1458cdcb943b766c27d8
Bartyee <bpawelec998@gmail.com>
2018-11-08 12:51:03 +0100

    Working app but not complet

--- app/js/script.js
+++ app/js/script.js
@@ -1,96 +1,21 @@
-var getCurrencyLogo = function(){
-    $.ajax({
-        url: 'https://chasing-coins.com/api/v1/std/logo/BTC'
-    })
-}
-
-var numberRound = function(data){
-
-    var convert = Math.round(data).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-    return convert
-}
+UIController = (function(){
 
 
-
-var getCurrencyList = function(){
-        $.ajax({
-        url: 'https://chasing-coins.com/api/v1/top-coins/200',
-        dataType: 'json',
-        type: 'get',
-
-    }).done(function(res){
-        
-        
-        // console.log(change7D);
-        // var additionalArray = [];
-        var dataArray = [];
-        
-        // console.log(additionalArray);
-        dataArray = Array.from(Object.keys(res), k=>res[k]);
-        dataArray.forEach((item,index) =>{
-
-            var row = document.createElement('tr');
-
-            
-            row.className = 'row-coin';
-            
-            row.innerHTML = `
-                <th scope='row'>${index}</th>
-                <td class='logoNameCoin'><img width='25px ' src='https://chasing-coins.com/api/v1/std/logo/${item.symbol}'/> <p>${item.symbol}</p></td>
-                <td><p>$ ${numberRound(item.cap)} </p></td>
-                <td><p>$ ${parseFloat(Math.round(item.price * 100) / 100).toFixed(2)}</p></td>
-                <td><p>${item.change.hour > 0 ? '<span style="color: green;">' + "+" + item.change.hour + '</span>' : '<span style="color: red;">' + item.change.hour}</span></p></td>
-                <td><p>${item.change.day > 0 ? '<span style="color: green;">' + "+" + item.change.day + '</span>' : '<span style="color: red;">' + item.change.day}</span> </p></td>
-                
-                
-            `
-
-             setTimeout(function(){
-                document.querySelector('.loading').style.display = 'none';
-                document.querySelector('.table-content').appendChild(row);
-             }, 4000)
-
-        });
-
-    });
-
-    
-}
-
-getCryptocurrencyGlobalInfo();
-getCurrencyList();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var UIController = (function(){
-
-
-    var DOMstrings = {
-        cryptoAmount: '.market-info__cryptoAmount',
-        marketsAmount: '.market-info__marketsAmount',
-        marketCap: '.market-info__marketCap',
-        dailyVolume: '.market-info__dailyVolume',
+    const DOMstrings = {
+        activeCrypto: '.market-info__activeCrypto--data',
+        marketCap: '.market-info__marketCap--data',
+        marketVolume24h: '.market-info__volume24h--data',
         navPrimary: '.nav-primary',
         navSecondary: '.nav-secondary',
         navHamburgerBtn: '.nav-hamburger',
+        logoNameCoin: '.logoNameCoin',
+        coinSymbol: '.coinSymbol',
+        coinMarketCap: '.coinMarketCap',
+        coinPrice: '.coinPrice',
+        coinHourChange: '.coinHourChange',
+        coinDayChange: '.coinDayChange',
+        coinWeekChange: '.coinWeekChange',
+        coinDailyVolume: '.coinDailyVolume'
     };
 
     return{
@@ -105,19 +30,127 @@ var UIController = (function(){
 
 })();
 
-var controller = (function(UIController){
+const controller = (function(UIController){
+
+    const getCurrencyLogo = () =>{
+        $.ajax({
+            url: 'https://chasing-coins.com/api/v1/std/logo/BTC'
+        })
+    }
     
-    var setupEventListeners = function(){
-        var DOM = UIController.getDOMStrings();
+    const numberRound = data => {
+    
+        let convert = Math.round(data).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        return convert
+    
+    }
+    
+    const getCurrencyList = () => {
+        let firstApi_dataArray = [];
+        let secondApi_dataArray = [];
+     
+        $.ajax({
+            url: 'https://chasing-coins.com/api/v1/top-coins/200',
+            dataType: 'json',
+            type: 'get',
+     
+        }).done(res => {
+            firstApi_dataArray = Object.values(res)
+            firstApi_dataArray.splice(100);
+            getSecondData();
+     
+        });
+     
+        const getSecondData = () => {
+            $.ajax({
+                url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+                dataType: 'json',
+                type: 'get',
+                
+                headers:{
+                    'X-CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7',
+                    'CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7'
+                }
+            }).done(res =>{
+                secondApi_dataArray = Object.values(res.data);
+                buildDOMCurrencyList(firstApi_dataArray, secondApi_dataArray);
+    
+                
+            })
+        }
+    
+        
+        const buildDOMCurrencyList = (arrOne,arrTwo) =>{
+    
+            arrOne.forEach((item,index) => {
+                let row = document.createElement('tr');
+                row.className = 'row-coin';
+                let secondApiArrayIndex = index;
+    
+               
+                row.innerHTML = `
+                    <th scope='row'>${index}</th>
+                    <td class="logoNameCoin"><img width='25px' class="logoCoin" src='https://chasing-coins.com/api/v1/std/logo/${item.symbol}'/> <a href="${item.url}"><p class="nameCoin">${secondApi_dataArray[secondApiArrayIndex].name}</p></a><button class="addCoinFavourite"</td>
+                    <td class="coinSymbol"><p>${item.symbol}</p></td>
+                    <td class="coinMarketCap"><p>$ ${numberRound(item.cap)} </p></td>
+                    <td class="coinPrice"><p>$ ${parseFloat(Math.round(item.price * 100) / 100).toFixed(2)}</p></td>
+                    <td class="coinHourChange"><p>${item.change.hour > 0 ? '<span style="color: #43D64E;">' + "+" + item.change.hour + ' %' + '</span>' : '<span style="color: #ff0000;">' + item.change.hour + ' %'}</span></p></td>
+                    <td class="coinDayChange"><p>${item.change.day > 0 ? '<span style="color: #43D64E;">' + "+" + item.change.day + ' %' + '</span>' : '<span style="color: #ff0000;">' + item.change.day + ' %'}</span</p></td>
+                    <td class="coinWeekChange">
+                    <p>
+                        ${
+                            secondApi_dataArray[secondApiArrayIndex].quote.USD.percent_change_7d > 0 ? '<span style="color: #43D64E;">' + '+' + parseFloat(Math.round(secondApi_dataArray[secondApiArrayIndex].quote.USD.percent_change_7d * 100) / 100).toFixed(2) + ' %' + '</span>' : '<span style="color: #ff0000;">' + parseFloat(Math.round(secondApi_dataArray[secondApiArrayIndex].quote.USD.percent_change_7d * 100) / 100).toFixed(2) + ' %' + '</span>'
+                        }
+                    </p>
+                    </td>
+                    <td class="coinDailyVolume">
+                    <p>
+                        ${numberRound(secondApi_dataArray[secondApiArrayIndex].quote.USD.volume_24h) + ' ' + secondApi_dataArray[secondApiArrayIndex].symbol}
+                    </p>
+                    </td>
+                `
+                    document.querySelector('.loading').style.display = 'none';
+                    document.querySelector('.table-content').appendChild(row);
+    
+                
+            });
+    
+            
+        }
+    }
+
+    const setupEventListeners = () => {
+        let DOM = UIController.getDOMStrings();
         
         document.querySelector(DOM.navHamburgerBtn).addEventListener('click', function(){
             $(DOM.navSecondary).toggle();
         });
     }
 
+    const getMarketInfo = () => {
+        $.ajax({
+            url: 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest',
+                dataType: 'json',
+                type: 'get',
+                
+            headers:{
+                'X-CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7',
+                'CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7'
+            }
+        }).done(res =>{
+            var DOM = UIController.getDOMStrings();
+            document.querySelector(DOM.activeCrypto).innerHTML = res.data.active_cryptocurrencies;
+            document.querySelector(DOM.marketCap).innerHTML = '$' + numberRound(res.data.quote.USD.total_market_cap);
+            document.querySelector(DOM.marketVolume24h).innerHTML = '$' + numberRound(res.data.quote.USD.total_volume_24h);
+        })
+    }
+
     return{
         init: function(){
             setupEventListeners();
+            getMarketInfo();
+            getCurrencyLogo();
+            getCurrencyList();
             console.log('Application has started');
         
         }
