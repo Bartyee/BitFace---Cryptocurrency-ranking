b69e3499f7bbae164d188b22cb44338f93a1eb63
Bartyee <bpawelec998@gmail.com>
2018-11-15 19:18:15 +0100

    fix

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,5 @@
 UIController = (function(){
 
-    
-
     let DOMstrings = {
         activeCrypto: '.market-info__activeCrypto--data',
         marketCap: '.market-info__marketCap--data',
@@ -20,7 +18,7 @@ UIController = (function(){
         coinDayChange: '.coinDayChange',
         coinWeekChange: '.coinWeekChange',
         coinDailyVolume: '.coinDailyVolume',
-        renderDone: false,
+        renderCurrencyList: false,
     };
 
     return{
@@ -32,8 +30,26 @@ UIController = (function(){
 })();
 
 const controller = (function(UIController){
-    
 
+    const getMarketInfo = () => {
+        let proxy = 'https://cors-anywhere.herokuapp.com/';
+        $.ajax({
+            url: proxy + 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest',
+                dataType: 'json',
+                type: 'get',
+                
+            headers:{
+                'X-CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7',
+                'CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7'
+            }
+        }).done(res =>{
+            let DOM = UIController.getDOMStrings();
+            document.querySelector(DOM.activeCrypto).innerHTML = res.data.active_cryptocurrencies;
+            document.querySelector(DOM.marketCap).innerHTML = '$' + numberRound(res.data.quote.USD.total_market_cap);
+            document.querySelector(DOM.marketVolume24h).innerHTML = '$' + numberRound(res.data.quote.USD.total_volume_24h);
+        })
+    }
+    
     const getCurrencyLogo = () =>{
         
         $.ajax({
@@ -85,92 +101,76 @@ const controller = (function(UIController){
         
         const buildDOMCurrencyList = (arrOne,arrTwo) =>{ //Two API arrays
 
+            let favouriteArray = []; // Array with favourite Coins
+
             let renderDom = false;
 
-            let favouriteCoinList = [];
-    
-            arrOne.forEach((item,index) => { //get arrOne items and from arrTwo
-                let row = document.createElement('tr');
-                row.className = 'row-coin';
-                row.setAttribute("id", index);
-                let secondApiArrayIndex = index;
-                
-                row.innerHTML = `
-                    <th scope='row'>${index}</th>
-                    <td class="logoNameCoin"><img width='25px' class="logoCoin" src='https://chasing-coins.com/api/v1/std/logo/${item.symbol}'/> <a href="${item.url}"><p class="nameCoin">${arrTwo[secondApiArrayIndex].name}</p></a><button class="addCoinFavourite"></button></td>
-                    <td class="coinSymbol"><p>${item.symbol}</p></td>
-                    <td class="coinMarketCap"><p>$ ${numberRound(item.cap)} </p></td>
-                    <td class="coinPrice"><p>$ ${parseFloat(Math.round(item.price * 100) / 100).toFixed(2)}</p></td>
-                    <td class="coinHourChange"><p>${item.change.hour > 0 ? '<span style="color: #43D64E;">' + "+" + item.change.hour + ' %' + '</span>' : '<span style="color: #ff0000;">' + item.change.hour + ' %'}</span></p></td>
-                    <td class="coinDayChange"><p>${item.change.day > 0 ? '<span style="color: #43D64E;">' + "+" + item.change.day + ' %' + '</span>' : '<span style="color: #ff0000;">' + item.change.day + ' %'}</span</p></td>
-                    <td class="coinWeekChange">
-                    <p>
-                        ${
-                            arrTwo[secondApiArrayIndex].quote.USD.percent_change_7d > 0 ? '<span style="color: #43D64E;">' + '+' + parseFloat(Math.round(arrTwo[secondApiArrayIndex].quote.USD.percent_change_7d * 100) / 100).toFixed(2) + ' %' + '</span>' : '<span style="color: #ff0000;">' + parseFloat(Math.round(arrTwo[secondApiArrayIndex].quote.USD.percent_change_7d * 100) / 100).toFixed(2) + ' %' + '</span>'
-                        }
-                    </p>
-                    </td>
-                    <td class="coinDailyVolume">
-                    <p>
-                        ${numberRound(arrTwo[secondApiArrayIndex].quote.USD.volume_24h) + ' ' + arrTwo[secondApiArrayIndex].symbol}
-                    </p>
-                    </td>
-                `
-                    document.querySelector('.loading').style.display = 'none';
-                    document.querySelector('.table-content').appendChild(row);
-
-
-                    if(index == arrOne.length - 1){
-                        renderDom = true;
-                    }
-
+                arrOne.forEach((item,index) => { //get arrOne items and from arrTwo
+                    let row = document.createElement('tr');
+                    row.className = 'row-coin';
+                    row.setAttribute("id", index);
+                    let secondApiArrayIndex = index;
                     
-                    // if (index == arrOne.length - 1){ //sprawdzam czy zakończono render wszystkich elementów z API
-                        
-                    //     let items = document.getElementsByClassName('addCoinFavourite'); // porbranie list elementów gdzie znajdują się buttony
-
-
-                    //     for(let i = 0; i<items.length; i++){ 
-                    //         items[i].addEventListener('click', function(){ //przypisanie do każdego elementu funkcji click
-                    //             let index = this.parentNode.parentNode.id;
-                    //             if(favouriteCoinList.includes(items[i].parentNode.parentNode))
-                    //             {
-                    //                 $(this).toggleClass("addCoinFavourite-green")
-                    //                 favouriteCoinList.splice(index,1)
-                    //                 console.log(favouriteCoinList)
-                    //             }
-                    //             else{
-                    //                 $(this).toggleClass("addCoinFavourite-green")
-                    //                 console.log(favouriteCoinList.push(this.parentNode.parentNode));
-                    //                 console.log(favouriteCoinList);
-                    //             }
-                    //             console.log(index);
-                    //         });
-                    //     }
-                    // }
-            });
-
+                    row.innerHTML = `
+                        <th scope='row'>${index+1}</th>
+                        <td class="logoNameCoin"><img width='25px' class="logoCoin" src='https://chasing-coins.com/api/v1/std/logo/${item.symbol}'/> <a href="${item.url}"><p class="nameCoin">${arrTwo[secondApiArrayIndex].name}</p></a><button class="addCoinFavourite"></button></td>
+                        <td class="coinSymbol"><p>${item.symbol}</p></td>
+                        <td class="coinMarketCap"><p>$ ${numberRound(item.cap)} </p></td>
+                        <td class="coinPrice"><p>$ ${parseFloat(Math.round(item.price * 100) / 100).toFixed(2)}</p></td>
+                        <td class="coinHourChange"><p>${item.change.hour > 0 ? '<span style="color: #43D64E;">' + "+" + item.change.hour + ' %' + '</span>' : '<span style="color: #ff0000;">' + item.change.hour + ' %'}</span></p></td>
+                        <td class="coinDayChange"><p>${item.change.day > 0 ? '<span style="color: #43D64E;">' + "+" + item.change.day + ' %' + '</span>' : '<span style="color: #ff0000;">' + item.change.day + ' %'}</span</p></td>
+                        <td class="coinWeekChange">
+                        <p>
+                            ${
+                                arrTwo[secondApiArrayIndex].quote.USD.percent_change_7d > 0 ? '<span style="color: #43D64E;">' + '+' + parseFloat(Math.round(arrTwo[secondApiArrayIndex].quote.USD.percent_change_7d * 100) / 100).toFixed(2) + ' %' + '</span>' : '<span style="color: #ff0000;">' + parseFloat(Math.round(arrTwo[secondApiArrayIndex].quote.USD.percent_change_7d * 100) / 100).toFixed(2) + ' %' + '</span>'
+                            }
+                        </p>
+                        </td>
+                        <td class="coinDailyVolume">
+                        <p>
+                            ${numberRound(arrTwo[secondApiArrayIndex].quote.USD.volume_24h) + ' ' + arrTwo[secondApiArrayIndex].symbol}
+                        </p>
+                        </td>
+                    `
+                        document.querySelector('.loading').style.display = 'none';
+                        document.querySelector('.table-content').appendChild(row);
+    
+    
+                        if(index == arrOne.length - 1){
+                            renderDom = true;
+                        }
+                });
+             
             if(renderDom == true){
-                var items = document.getElementsByClassName('addCoinFavourite');
-                console.log(items);
+                let buttonArray = [...document.getElementsByClassName('addCoinFavourite')];
 
-                for(let i = 0; i<items.length; i++){
-                    items[i].addEventListener('click', function(){
-                        console.log('click');
-                        let index = this.parentNode.parentNode.id;
-                        if(favouriteCoinList.includes(items[i].parentNode.parentNode)){
-                            $(items[i]).toggleClass("addCoinFavourite-green");
-                            favouriteCoinList.splice(index,1)
-                            console.log(favouriteCoinList)
+                buttonArray.forEach((item) => {
+                    item.addEventListener('click',function(){
+                        
+                        let index = favouriteArray.indexOf(item.parentElement.parentElement)
+                        
+                        
+                        if(favouriteArray.includes(item.parentElement.parentElement)){
+                            $(item).toggleClass('addCoinFavourite-green');
+                            if(index > -1){
+                                favouriteArray.splice(index, 1);
+                                console.log(favouriteArray);
+                            }
+                            
+
                         }
                         else{
-                            $(items[i]).toggleClass("addCoinFavourite-green")
-                            console.log(favouriteCoinList.push(this.parentNode.parentNode));
-                            console.log(favouriteCoinList);
+                            console.log('Id dodanego rowa ' + item.parentElement.parentElement.id);
+                            favouriteArray.push(item.parentElement.parentElement);
+                            $(item).toggleClass('addCoinFavourite-green');
+                            console.log(favouriteArray);
                         }
+
                     })
-                }
+                })
             }
+
+            
         }
     }
 
@@ -200,31 +200,6 @@ const controller = (function(UIController){
         document.querySelector(DOM.searchInput).addEventListener('keyup',searchCoin);
     }
 
-    const remove = (array,element) => {
-        return array.filter(e => e !== element)
-    }
-
-    
-
-    const getMarketInfo = () => {
-        let proxy = 'https://cors-anywhere.herokuapp.com/';
-        $.ajax({
-            url: proxy + 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest',
-                dataType: 'json',
-                type: 'get',
-                
-            headers:{
-                'X-CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7',
-                'CMC_PRO_API_KEY': '90a88319-e5af-4f4b-9307-8c1bec2000b7'
-            }
-        }).done(res =>{
-            let DOM = UIController.getDOMStrings();
-            document.querySelector(DOM.activeCrypto).innerHTML = res.data.active_cryptocurrencies;
-            document.querySelector(DOM.marketCap).innerHTML = '$' + numberRound(res.data.quote.USD.total_market_cap);
-            document.querySelector(DOM.marketVolume24h).innerHTML = '$' + numberRound(res.data.quote.USD.total_volume_24h);
-        })
-    }
-
     return{
         init: function(){
             findCoinInput();
@@ -232,7 +207,6 @@ const controller = (function(UIController){
             getCurrencyLogo();
             getCurrencyList();
             console.log('Application has started');
-        
         }
     }
 
